0ebd3cdd477f8132b5ba34971c3f246403b5b797
Gavin Eccles <ecclesg@tcd.ie>
2017-10-11 11:28:42 +0100

    Committ 2

--- src/Practice.java
+++ src/Practice.java
@@ -4,6 +4,15 @@ public class Practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello World");
+		int count = 0;
+		for(int i=1; i<5; i++){
+			if(i%2==0){
+				count++;
+			}
+		}
+		if(count==2){
+			System.out.print("Correct");
+		}
 	}
 
 }
